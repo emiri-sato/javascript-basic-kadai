50b7e2fe0fb4804de4b7f9969e6eb089f14d38a4
emiri-sato <emiri.612@outlook.jp>
2025-09-24 14:51:14 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,11 +1,12 @@
 let num=117;
 
 if(num%3 == 0 && num%5==0){
-    console.log(num +"は3と5の倍数です");
+    console.log("3と5の倍数です");
 }else if(num%5 == 0){
-    console.log(num +"は5の倍数です");
+    console.log("5の倍数です");
 }else if(num%3==0 ){
-    console.log(num +"は3の倍数です");
+    console.log("3の倍数です");
 }else{
     console.log(num);
+
 }
